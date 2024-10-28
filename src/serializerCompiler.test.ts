f1eed48c1777fa3702d897bfb9ae4761c42e1b4c
samchungy <samchungy@gmail.com>
2024-10-28 21:14:57 +1100

    add dumb test

--- src/serializerCompiler.test.ts
+++ src/serializerCompiler.test.ts
@@ -139,6 +139,145 @@ describe('serializerCompiler', () => {
 
     expect(result.json()).toEqual({ jobId: 'foo' });
   });
+
+  it('should handle a complex response', async () => {
+    const app = fastify();
+
+    app.setSerializerCompiler(serializerCompiler);
+    app.withTypeProvider<FastifyZodOpenApiTypeProvider>().post(
+      '/',
+      {
+        schema: {
+          response: {
+            200: {
+              content: {
+                'application/json': {
+                  schema: z.object({
+                    // invent a complex schema
+                    jobId: z.string().openapi({
+                      description: 'Job ID',
+                      example: '60002023',
+                    }),
+                    jobName: z.string().openapi({
+                      description: 'Job Name',
+                      example: 'Job 1',
+                    }),
+                    jobStatus: z.string().openapi({
+                      description: 'Job Status',
+                      example: 'completed',
+                    }),
+                    jobDetails: z.object({
+                      jobType: z.string().openapi({
+                        description: 'Job Type',
+                        example: 'export',
+                      }),
+                      jobDate: z.string().openapi({
+                        description: 'Job Date',
+                        example: '2021-09-01',
+                      }),
+                    }),
+                    jobArray: z.array(
+                      z
+                        .object({
+                          jobType: z.string().openapi({
+                            description: 'Job Type',
+                            example: 'export',
+                          }),
+                          jobDate: z.string().openapi({
+                            description: 'Job Date',
+                            example: '2021-09-01',
+                          }),
+                        })
+                        .openapi({ ref: 'something' }),
+                    ),
+                    jobTuple: z
+                      .tuple([
+                        z.string().openapi({ ref: 'string' }),
+                        z.number().openapi({ ref: 'number' }),
+                      ])
+                      .openapi({
+                        description: 'Job Tuple',
+                        example: ['foo', 123],
+                      }),
+                    metadata: z.discriminatedUnion('type', [
+                      z
+                        .object({
+                          type: z.literal('success'),
+                          success: z.string().openapi({
+                            description: 'Success Message',
+                            example: 'Job completed successfully',
+                          }),
+                        })
+                        .openapi({ ref: 'success' }),
+                      z
+                        .object({
+                          type: z.literal('error'),
+                          error: z.string().openapi({
+                            description: 'Error Message',
+                            example: 'Job failed',
+                          }),
+                        })
+                        .openapi({ ref: 'error' }),
+                    ]),
+                  }),
+                },
+              },
+            },
+          } satisfies ZodOpenApiResponsesObject,
+        },
+      },
+      async (_req, res) =>
+        res.send({
+          jobId: '60002023',
+          jobName: 'Job 1',
+          jobStatus: 'completed',
+          jobDetails: {
+            jobType: 'export',
+            jobDate: '2021-09-01',
+          },
+          jobArray: [
+            {
+              jobType: 'export',
+              jobDate: '2021-09-01',
+            },
+          ],
+          jobTuple: ['foo', 123],
+          metadata: {
+            type: 'success',
+            success: 'Job completed successfully',
+          },
+        }),
+    );
+    await app.ready();
+
+    const result = await app.inject().post('/');
+
+    expect(result.json()).toMatchInlineSnapshot(`
+{
+  "jobArray": [
+    {
+      "jobDate": "2021-09-01",
+      "jobType": "export",
+    },
+  ],
+  "jobDetails": {
+    "jobDate": "2021-09-01",
+    "jobType": "export",
+  },
+  "jobId": "60002023",
+  "jobName": "Job 1",
+  "jobStatus": "completed",
+  "jobTuple": [
+    "foo",
+    123,
+  ],
+  "metadata": {
+    "success": "Job completed successfully",
+    "type": "success",
+  },
+}
+`);
+  });
 });
 
 describe('createSerializerCompiler', () => {
