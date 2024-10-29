657726117093a1a788c05359fdf58d2b0b5ab09a
Sam Chung <samchungy@gmail.com>
2024-10-29 11:58:20 +1100

    Add Fast Json Stringify (#179)

--- src/serializerCompiler.ts
+++ src/serializerCompiler.ts
@@ -1,7 +1,51 @@
+import fastJsonStringify, {
+  type ObjectSchema,
+  type Schema,
+} from 'fast-json-stringify';
 import type { FastifySerializerCompiler } from 'fastify/types/schema';
-import type { ZodType } from 'zod';
+import type { ZodType, ZodTypeAny } from 'zod';
+import { createSchema } from 'zod-openapi';
 
 import { ValidationError } from './validationError';
+
+export interface SerializerOptions {
+  components?: Record<string, ZodTypeAny>;
+  stringify?: (value: unknown) => string;
+}
+
+export const createSerializerCompiler =
+  (opts?: SerializerOptions): FastifySerializerCompiler<ZodType> =>
+  ({ schema }) => {
+    let stringify = opts?.stringify;
+    if (!stringify) {
+      const { schema: jsonSchema, components } = createSchema(schema, {
+        components: opts?.components,
+        componentRefPath: '#/definitions/',
+      });
+
+      const maybeDefinitions: Pick<ObjectSchema, 'definitions'> | undefined =
+        components
+          ? {
+              definitions: components as Record<string, Schema>,
+            }
+          : undefined;
+
+      stringify = fastJsonStringify({
+        ...(jsonSchema as Schema),
+        ...maybeDefinitions,
+      });
+    }
+
+    return (value) => {
+      const result = schema.safeParse(value);
+
+      if (!result.success) {
+        throw new ValidationError(result.error, 'response');
+      }
+
+      return stringify(result.data);
+    };
+  };
 
 /**
  * Enables zod-openapi schema response validation
@@ -13,14 +57,4 @@ import { ValidationError } from './validationError';
  * const server = Fastify().setserializerCompiler(serializerCompiler)
  * ```
  */
-export const serializerCompiler: FastifySerializerCompiler<ZodType> =
-  ({ schema }) =>
-  (value) => {
-    const result = schema.safeParse(value);
-
-    if (!result.success) {
-      throw new ValidationError(result.error, 'response');
-    }
-
-    return JSON.stringify(result.data);
-  };
+export const serializerCompiler = createSerializerCompiler();
